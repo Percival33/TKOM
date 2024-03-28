2fc95b25d9b5d9a82e681234e56b7320be153fd5
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-03-28 14:51:23 +0100

    fix: temp

---
+++ src/main/java/org/siu/Main.java
@@ -0,0 +1,12 @@
+package org.siu;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main
+{
+    public static void main(final String[] args)
+    {
+        log.info("HELLO");
+        log.warn("hello");
+    }
+}
