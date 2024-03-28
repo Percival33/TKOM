90ba44f30d8d7099ef0fd678fe1e2406c6048742
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-03-28 14:50:36 +0100

    fix: temp

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "org.siu"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    implementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.20.0")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
