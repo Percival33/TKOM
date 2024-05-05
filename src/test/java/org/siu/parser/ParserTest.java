ed2e361725a5db022ae664627a6bb20592823a52
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-05-06 00:52:41 +0200

    chore: refactor test

--- src/test/java/org/siu/parser/ParserTest.java
+++ src/test/java/org/siu/parser/ParserTest.java
@@ -1,9 +1,11 @@
 package org.siu.parser;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.siu.ast.Argument;
 import org.siu.ast.Program;
+import org.siu.ast.expression.Expression;
 import org.siu.ast.expression.FunctionCallExpression;
 import org.siu.ast.expression.arithmetic.AddArithmeticExpression;
 import org.siu.ast.expression.arithmetic.ModuloArithmeticExpression;
@@ -17,59 +19,52 @@ import org.siu.lexer.Lexer;
 import org.siu.lexer.LexerImpl;
 import org.siu.token.Position;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ParserTest {
-    private final ErrorHandler errorHandler = Mockito.mock(ErrorHandler.class);
-    private final Position position = Mockito.mock(Position.class);
+    private ErrorHandler errorHandler;
+    private Position position;
 
-    Lexer toLexer(String text) {
-        return new LexerImpl(text, errorHandler);
+    @BeforeEach
+    void setUp() {
+        errorHandler = Mockito.mock(ErrorHandler.class);
+        position = Mockito.mock(Position.class);
     }
 
     Parser toParser(String text) {
-        Lexer lexer = toLexer(text);
+        Lexer lexer = new LexerImpl(text, errorHandler);
         return new Parser(lexer, errorHandler);
     }
 
+    private DeclarationStatement createDeclaration(String name, ValueType type, Expression expression) {
+        Argument argument = new Argument(type, name);
+        return new DeclarationStatement(argument, expression, position);
+    }
+
     @Test
-    void addArithmeticOperation() throws Exception {
+    void addArithmeticOperation() {
         String s = "int a = 2 * 1;";
         Parser parser = toParser(s);
         Program program = parser.buildProgram();
-        assertEquals(Map.of("a", new DeclarationStatement(new Argument(ValueType.INT, "a"), new MultiplyArithmeticExpression(new IntegerExpression(2, position), new IntegerExpression(1, position), position), position)), program.getDeclarations());
+
+        Expression expression = new MultiplyArithmeticExpression(new IntegerExpression(2, position), new IntegerExpression(1, position), position);
+
+        assertEquals(Map.of("a", createDeclaration("a", ValueType.INT, expression)), program.getDeclarations());
     }
 
     @Test
     void moduloAddArithmeticExpressionsTest() throws Exception {
-        String s = "int a = 10 % f(33, 66) + 3;";
+        String s = "float a = 10 % f(33, 66) + 3;";
         Parser parser = toParser(s);
         Program program = parser.buildProgram();
-        assertEquals(new DeclarationStatement(
-                        new Argument(ValueType.INT, "a"),
-                        new AddArithmeticExpression(
-                                new ModuloArithmeticExpression(
-                                        new IntegerExpression(10, position),
-                                        new FunctionCallExpression(
-                                                "f",
-                                                List.of(
-                                                        new IntegerExpression(33, position),
-                                                        new IntegerExpression(66, position)
-                                                ),
-                                                position
-                                        ),
-                                        position
-                                ),
-                                new IntegerExpression(3, position),
-                                position
-                        ),
-                        position
-                ),
-                program.getDeclarations().get("a"));
+        Expression right = new FunctionCallExpression("f", List.of(new IntegerExpression(33, position), new IntegerExpression(66, position)), position);
+        Expression modulo = new ModuloArithmeticExpression(new IntegerExpression(10, position), right, position);
+        Expression add = new AddArithmeticExpression(modulo, new IntegerExpression(3, position), position);
+
+        assertEquals(createDeclaration("a", ValueType.FLOAT, add), program.getDeclarations().get("a"));
     }
 
     @Test
@@ -77,32 +72,11 @@ class ParserTest {
         String s = "int b = 3 - 1 * 5 % 2 + 10;";
         Parser parser = toParser(s);
         Program program = parser.buildProgram();
-        assertEquals(new DeclarationStatement(
-                        new Argument(ValueType.INT, "b"),
-                        new AddArithmeticExpression(
-                                new SubtractArithmeticExpression(
-                                        new IntegerExpression(3, position),
-                                        new ModuloArithmeticExpression(
-                                                new MultiplyArithmeticExpression(
-                                                        new IntegerExpression(1, position),
-                                                        new IntegerExpression(5, position),
-                                                        position
-                                                ),
-                                                new IntegerExpression(2, position),
-                                                position
-                                        ),
-                                        position
-                                ),
-                                new IntegerExpression(10, position),
-                                position
-                        ),
-                        position
-                ),
-                program.getDeclarations().get("b"));
-    }
+        Expression multiply = new MultiplyArithmeticExpression(new IntegerExpression(1, position), new IntegerExpression(5, position), position);
+        Expression modulo = new ModuloArithmeticExpression(multiply, new IntegerExpression(2, position), position);
+        Expression subtract = new SubtractArithmeticExpression(new IntegerExpression(3, position), modulo, position);
+        Expression add = new AddArithmeticExpression(subtract, new IntegerExpression(10, position), position);
 
-    @Test
-    public void a() {
-//        "int a = 1 + 2 * 3;"              || new Program(Map.of(), Map.of("a", new DeclarationStatement(new Argument("a", new TypeDeclaration(ValueType.INTEGER)), new AddArithmeticExpression(new IntegerExpression(1, position), new MultiplyArithmeticExpression(new IntegerExpression(2, position), new IntegerExpression(3, position), position), position), position)))
+        assertEquals(createDeclaration("b", ValueType.INT, add), program.getDeclarations().get("b"));
     }
 }
